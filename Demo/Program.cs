cab14c19e7c4bf7669ab5acee7ac07e198decffe
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-09 20:06:19 +0200

    Abstaction

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,10 +1,20 @@
-﻿namespace Demo
+﻿using Demo.abstraction;
+using System.Drawing;
+
+namespace Demo
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
-        }
+           
+           //  Rectangle rectangle = new Rectangle() { Dim01 = 12, Dim02 = 12 };
+           //
+           // Console.WriteLine($"Area: {rectangle.GetArea()}");
+           // Console.WriteLine($"Perimeter: {rectangle.Perimeter}");
+
+
+
+        }  
     }
 }
---
+++ Demo/abstraction/Shape.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo.abstraction
+{
+    internal abstract class Shape
+    {
+        public double Dim01 { get; set; }
+        public double Dim02 { get; set; }
+
+        public abstract double GetArea();
+        public abstract double Perimeter { get; }
+    }
+}
