2abeea4023e3c0eb7f78c06fab772908c10f3e72
amiraabdelazez <amiraabdelazez2@gmail.com>
2025-01-09 21:00:24 +0200

    operator overloading

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Demo.abstraction;
+using Demo.Operator_Overloading;
 using System.Drawing;
 
 namespace Demo
@@ -7,14 +8,44 @@ namespace Demo
     {
         static void Main(string[] args)
         {
-           
-           //  Rectangle rectangle = new Rectangle() { Dim01 = 12, Dim02 = 12 };
-           //
-           // Console.WriteLine($"Area: {rectangle.GetArea()}");
-           // Console.WriteLine($"Perimeter: {rectangle.Perimeter}");
 
+            #region abstraction
+            //  Rectangle rectangle = new Rectangle() { Dim01 = 12, Dim02 = 12 };
+            //
+            // Console.WriteLine($"Area: {rectangle.GetArea()}");
+            // Console.WriteLine($"Perimeter: {rectangle.Perimeter}");
+            //
+            // Square square = new Square() { Dim01 = 1, Dim02 = 2, };
+            // Console.WriteLine(square.Perimeter);
+            // Console.WriteLine(square.GetArea()); 
+            #endregion
+            #region operator overloading
+            //
+            //Complex C01 = new Complex() { Real = 3, Imag = 4 };
+            //Complex C02 = new Complex() {Real = 4, Imag = 5 };
 
+            //Console.WriteLine(C01);
+            //Console.WriteLine(C02);
+            //
+            //Complex C03 = C01 + C02;
+            //Console.WriteLine(C03);
+            //
+            //C03 = C01 - C02;
+            //Console.WriteLine(C03);
 
-        }  
+            //C01++;
+            //Console.WriteLine(C01);
+            //
+            //C01--;
+            //Console.WriteLine(C01);
+
+            //if (C01 > C02)
+            //{
+            //    Console.WriteLine("C01");
+            //}
+            //else
+            //    Console.WriteLine("C02"); 
+            #endregion
+        }   
     }
 }
